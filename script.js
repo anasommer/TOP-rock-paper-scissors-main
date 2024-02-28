3c945b6a46ed93ca61a770591aabfe036aebda2b
Ana Sommer <anastassia.sommer@gmail.com>
2024-02-28 13:05:59 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// TODO: write a function 'getComputerChoice' that will randomly return either 'Rock', 'Paper', or 'Scissors'
+
+/* 
+TODO: write a function that plays a single round called 'playRound'
+TODO: It should take 2 parameters: playerSection and computerSection 
+TODO: It then should return a string that declares the winner or tie of the round like so: "You Lose! Paper beats Rock"
+- make sure that function's playerSelection parameter case-insensitive
+*/
+
+/* 
+TODO: write another function called 'playGame' and use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end
+
+- call playRound function 5 times in a row
+- use console.log() to display the results of each round and the winner at the end
+- use prompt() to get input from the user
+
+*/
